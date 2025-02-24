5b64e36ae673d1b9ceec32bcc7e62fcbaeaf5227
DanielMikitiuk <dani4ku@gmail.com>
2025-02-24 15:49:03 +0100

    add working calculator without design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,74 @@
-console.log('Hello, world!');
+const display = document.getElementById('display');
+const buttons = document.getElementById('buttons');
+
+const data = [ //data of the calculator
+
+    ['(',')','<='],
+    ['7', '8', '9', '/'],
+    ['4', '5', '6', '*'],
+    ['1', '2', '3', '-'],
+    ['C', '0', '=', '+']
+];
+
+//create elements
+data.forEach((row) => {
+    const rowElement = document.createElement('div');
+    buttons.append(rowElement);
+    rowElement.classList.add('row-container');
+
+    row.forEach((button) => {
+        const btnElement = document.createElement('button');
+        rowElement.append(btnElement);
+        btnElement.innerText = button;
+
+        //add action for every button
+        btnElement.addEventListener('click', () => handleClick(button));
+    });
+});
+
+let result = '';
+const operators = ['+', '-', '*', '/'];
+const values = ['1','2','3', '4', '5', '6', '7', '8', '9', '0', '(', ')'];
+function handleClick(button){
+    let lastChar = result.slice(-1);
+
+    if(button === '='){ //do the math
+        try{
+            result = eval(result).toString();
+        }catch(err){
+            result = 'error';
+        }
+        display.innerText = result;
+        return;
+    }
+
+    if (button === 'C'){  //reset calculator
+        result = '0';
+        display.innerText = result;
+        return;
+    }
+
+    if(result === '0' || result === 'error'){ //if we have 0 or error we change the result instead of concatenating
+            result = button;
+            display.innerText = result;
+            return;
+    }
+
+    if (button === '<='){ //delete one character
+        result = result.slice(0, -1);
+        display.innerText = result;
+        return;
+    }
+
+    if(operators.includes(lastChar)){ //check if we repeat the operator
+        if(!values.includes(button)){ //if the button that we click isn't value that we want we replace it
+            result = result.slice(0, -1) + button;
+            display.innerText = result;
+            return;
+        }
+    }
+
+    result += button;
+    display.innerText = result;
+
+}
